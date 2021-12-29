c8d44a69ff2e4e6cb48ea58dc7f54efa790e4ace
Me <vendramin.mitja@gmail.com>
2021-12-29 15:50:06 +0100

    initial commit

---
+++ src/test/java/si/fri/rso/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package si.fri.rso.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
